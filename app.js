fbc50b12c3543a73d9e51e555af9955bf9b91221
slatslime333 <75232128+slatslime333@users.noreply.github.com>
2026-08-09 04:02:57 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4179,9 +4179,77 @@ function showVS(){
 
     generateCPUCombo();
 
-    assignStadiumSides();
+    const app=document.getElementById("app");
 
-    showLaunchScreen();
+    app.innerHTML=`
+
+    <div class="background"></div>
+
+    <main class="menu">
+
+        <section class="menu-card">
+
+            <h1>ROUND ${Game.round}</h1>
+
+            <p>First to 7 Points</p>
+
+            <hr>
+
+            <div class="versus">
+
+                <div>
+
+                    <h2>YOU</h2>
+
+                    <p>
+                        ${Game.player.blade.name}<br>
+                        ${Game.player.ratchet.name}<br>
+                        ${Game.player.bit.name}
+                    </p>
+
+                    <h3>OVR ${Game.player.overall}</h3>
+
+                </div>
+
+                <div>
+
+                    <h2>CPU</h2>
+
+                    <p>
+                        ${Game.cpu.blade.name}<br>
+                        ${Game.cpu.ratchet.name}<br>
+                        ${Game.cpu.bit.name}
+                    </p>
+
+                    <h3>OVR ${Game.cpu.overall}</h3>
+
+                </div>
+
+            </div>
+
+            <br>
+
+            <button
+                class="menu-btn gold"
+                id="continueVS">
+
+                CONTINUE
+
+            </button>
+
+        </section>
+
+    </main>
+
+    `;
+
+    document.getElementById("continueVS").onclick=()=>{
+
+        assignStadiumSides();
+
+        showLaunchScreen();
+
+    };
 
 }
 
