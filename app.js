0e332ef7d70f6dd88980b038fbc867b7636fd0c6
slatslime333 <75232128+slatslime333@users.noreply.github.com>
2026-08-19 01:42:32 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1151,7 +1151,7 @@ function renderMainMenu(){
                 <span class="feature-icon">◎</span>
                 <div><b>REAL COMBO STATS</b><small>Blade × ratchet × height × Bit synergy</small></div>
             </div>
-            <div class="menu-version">V53 · STAT &amp; SYSTEM CLEANUP</div>
+            <div class="menu-version">V13 · STAT &amp; SYSTEM CLEANUP</div>
         </section>
     </main>`;
 }
